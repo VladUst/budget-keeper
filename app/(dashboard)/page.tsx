44957d35fe5db10eb9ba34bd0b71c1daf1005e53
VladUs <vladustyugo@gmail.com>
2024-06-15 23:49:57 +0700

    feat: add sign-up and sign-in forms

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-  return (
-    <Button>Click</Button>
-  );
+    return <UserButton afterSignOutUrl="/" />;
 }
