10edb41f75f17d6cf6b04ea86adcd97a938e595e
VladUs <vladustyugo@gmail.com>
2024-06-23 23:43:50 +0700

    feat: main layout and drizzle ORM

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,22 +1,13 @@
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
-import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { z } from 'zod';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.get('/hello', clerkMiddleware(), (c) => {
-    const auth = getAuth(c);
-
-    if (!auth?.userId) {
-        return c.json({ error: 'Unauthorized' });
-    }
+app.get('/hello', (c) => {
     return c.json({
-        message: 'Hello Next.js!',
-        userId: auth.userId,
+        message: 'Hello Next.js!'
     });
 });
 
